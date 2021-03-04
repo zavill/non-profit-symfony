36b27150676dcb79de1c21c7a02d4cf0a26e29b4
Daniil Zavilohin <denik6740@gmail.com>
2021-03-04 15:27:11 +0300

    Добавлены маршруты для новых страниц

--- src/Controller/Main/HomeController.php
+++ src/Controller/Main/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Main;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends BaseController
@@ -9,13 +10,39 @@ class HomeController extends BaseController
 
     /**
      * @Route("/", name="home")
+     *
+     * @return Response
      */
 
-    public function index(): \Symfony\Component\HttpFoundation\Response
+    public function index(): Response
     {
         $arRender = parent::renderDefault();
 
         return $this->render('main/index.html.twig', $arRender);
+    }
+
+    /**
+     * @Route("/about/", name="about")
+     *
+     * @return Response
+     */
+    public function about(): Response
+    {
+        $arRender = parent::renderDefault();
+
+        return $this->render('main/about.html.twig', $arRender);
+    }
+
+    /**
+     * @Route("/prayer/", name="prayer")
+     *
+     * @return Response
+     */
+    public function prayer(): Response
+    {
+        $arRender = parent::renderDefault();
+
+        return $this->render('main/prayer.html.twig', $arRender);
     }
 
 }
