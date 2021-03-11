3da0bf99b77087efba75a0a604648f7922600735
Daniil Zavilohin <denik6740@gmail.com>
2021-03-11 15:58:32 +0300

    Создан обработчик формы на странице "Prayer Request"

--- src/Controller/Main/PrayingController.php
+++ src/Controller/Main/PrayingController.php
@@ -6,6 +6,7 @@ namespace App\Controller\Main;
 
 use App\Entity\Praying;
 use App\Form\PrayingType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,14 +15,29 @@ class PrayingController extends BaseController
     /**
      * @Route("/prayer/", name="prayer")
      *
+     * @param Request $request
      * @return Response
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $arRender = parent::renderDefault();
 
+        /* Создаём форму и обрабатываем её */
         $praying = new Praying();
         $form = $this->createForm(PrayingType::class, $praying);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->createPrayingAction($praying);
+            $arRender['result'] = 'Your request has been successfully sent';
+
+            /* Сбрасываем значения формы и создаём новую */
+            unset($form, $praying);
+            $praying = new Praying();
+            $form = $this->createForm(PrayingType::class, $praying);
+        } else if ($form->isSubmitted() && !$form->isValid()) {
+            $arRender['result'] = 'Make sure that all fields of the form are filled out correctly';
+        }
 
         $arRender['form'] = $form->createView();
 
@@ -30,10 +46,12 @@ class PrayingController extends BaseController
 
     /**
      * Добавление заявки на молитву
-     * @Route("/prayer/create", methods={"POST"})
+     * @param Praying $praying
      */
-    public function createPraying()
+    public function createPrayingAction(Praying $praying)
     {
-
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($praying);
+        $entityManager->flush();
     }
 }
